f143875ba2a61f27b330b8db09a5f0c7df9e382e
Simone Brin <simonebrin@gmail.com>
2022-08-08 11:39:03 -0400

    added first questions and licenses

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
+const fs = require("fs")
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title for this project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your description for this project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license would you like to use?",
+        choices: ["None", "MIT", "Apache", "Boost", "BDS 3-Clause"]
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, function(err){
+        console.log(data)
+        if(err){
+            console.log(err)
+        } else {
+            console.log("You have successfuolly created your readme.")
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function(data){
+        writeToFile("README.md", generateMarkdown(data))
+        console.log(data)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,31 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  const chosenLicense = data.license
+  let licenseBadge = ""
+  if (chosenLicense === "None"){
+    return licenseBadge;
+  } 
+  if (chosenLicense === "MIT"){
+    licenseBadge = "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    return licenseBadge;
+  }
+  if (chosenLicense === "Apache"){
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      return licenseBadge;
+  }
+  if (chosenLicense === "Boost"){
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+      return licenseBadge;
+  }
+  if (chosenLicense === "BDS 3-Clause"){
+    licenseBadge =
+      "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+      return licenseBadge;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,6 +38,7 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+# ${data.description}
 
 `;
 }
